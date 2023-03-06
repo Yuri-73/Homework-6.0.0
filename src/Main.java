2d5f6600551294e6459ab7d5c023c3b954bf3c24
Юрий <fedejkin@gmail.com>
2023-03-06 21:51:28 +0300

    Task-1, 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1 () {
        System.out.println("Задача 1: Написать 4 строки с данными сотрудника");
@@ -9,5 +10,10 @@ public class Main {
        String lastName = "Ivanov ";
        String fullName = lastName + firstName + middleName;
        System.out.println("fullName = " + fullName);
+    }
+    public static void task2 () {
+        System.out.println("Задача 2: Изменение написания Ф.И.О. на полностью заглавные буквы");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(fullName.toUpperCase());
     }
 }
