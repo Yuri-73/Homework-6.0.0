12dd49cc1e4c95d3d8502aa7d13fbd0b1f1fa6a8
Юрий <fedejkin@gmail.com>
2023-03-06 22:20:53 +0300

    Task-1, 2, 3 Mod

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
     }
     public static void task1 () {
        System.out.println("Задача 1: Написать 4 строки с данными сотрудника");
-       String firstName = "Ivan ";
+       String firstName = "Ivan";
        String middleName = "Ivanovich";
-       String lastName = "Ivanov ";
-       String fullName = lastName + firstName + middleName;
+       String lastName = "Ivanov";
+       String fullName = lastName + ' ' + firstName + ' ' + middleName;
        System.out.println("ФИО сотрудника: " + fullName);
     }
     public static void task2 () {
