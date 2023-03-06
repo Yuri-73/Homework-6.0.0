6b7242ae85d23398af50d808b3fc931d67524305
Юрий <fedejkin@gmail.com>
2023-03-06 21:44:09 +0300

    Task-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+       System.out.println("Задача 1: Написать 4 строки с данными сотрудника");
+       String firstName = "Ivan ";
+       String middleName = "Ivanovich";
+       String lastName = "Ivanov ";
+       String fullName = lastName + firstName + middleName;
+       System.out.println("fullName = " + fullName);
     }
 }
