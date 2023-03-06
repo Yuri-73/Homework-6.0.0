2edb5dd01c8b09f1b951ffaa4379a22584c4ca66
Юрий <fedejkin@gmail.com>
2023-03-06 22:01:30 +0300

    Task-1, 2, 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1 () {
        System.out.println("Задача 1: Написать 4 строки с данными сотрудника");
@@ -9,11 +10,17 @@ public class Main {
        String middleName = "Ivanovich";
        String lastName = "Ivanov ";
        String fullName = lastName + firstName + middleName;
-       System.out.println("fullName = " + fullName);
+       System.out.println("ФИО сотрудника: " + fullName);
     }
     public static void task2 () {
         System.out.println("Задача 2: Изменение написания Ф.И.О. на полностью заглавные буквы");
         String fullName = "Ivanov Ivan Ivanovich";
         System.out.println(fullName.toUpperCase());
+    }
+    public static void task3 () {
+        System.out.println("Задача 3: Замена в строке символа 'ё' на символ 'е'");
+        String fullName = "Горбунков Семён Семёнович";
+        System.out.println("ФИО сотрудника с буквами 'ё': " + fullName);
+        System.out.println("Данные ФИО сотрудника после исправления: " + fullName.replace("ё", "e"));
     }
 }
